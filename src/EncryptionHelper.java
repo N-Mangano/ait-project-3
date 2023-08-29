2b0d9ab1e85437d6eee79b0b2a47bdd1c07fa5ac
Viktoriia Shcherbyna <silime7@gmail.com>
2023-08-30 00:26:21 +0200

    fix: add encrypt decrypt

--- src/EncryptionHelper.java
+++ src/EncryptionHelper.java
@@ -9,21 +9,32 @@ public class EncryptionHelper {
     encryptionFunction.put(CESAR_NAME + "encrypt", s-> {
       StringBuilder res=new StringBuilder();
       for(char ch:s.toCharArray()){
-        res.append(ch+3)
+        res.append(ch+3);
+      }
+      return res.toString();
+    });
+    encryptionFunction.put(CESAR_NAME + "decrypt", s-> {
+      StringBuilder res=new StringBuilder();
+      for(char ch:s.toCharArray()){
+        res.append(ch-3);
       }
       return res.toString();
     });
 
-
-    encryptionFunction.put(CESAR_NAME + "decrypt", s-> "hello");
-
   }
 
-  public static String encrypt(EncryptionMethods methods, String text){
-    return "";
+  public static String encrypt(EncryptionMethods method, String text) {
+    return switch (method) {
+      case CESAR -> encryptionFunction.get(method.getSimpleName() + "encrypt").process(text);
+      case VISIONERE -> "";
+    };
   }
-  public static String decrypt(EncryptionMethods methods, String text){
-    return "";
+
+  public static String decrypt(EncryptionMethods method, String text){
+     return switch (method){
+        case CESAR -> encryptionFunction.get(method.getSimpleName()+"decrypt").process(text);
+        case VISIONERE -> "";
+      };
   }
 
 }
