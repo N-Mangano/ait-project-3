96779ac4e4463858bff63b664cf477f7947c3a62
Nadezda Mangano <nadenka24@yandex.ru>
2023-09-01 21:27:25 +0200

    fix: add encrypt method

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -4,6 +4,15 @@ public class Vigenere {
   public Vigenere(String key) {
     this.key = key;
   }
+  public String encrypt(String text) {
+    StringBuilder encryptedText = new StringBuilder();
+    int keyIndex = 0;
+
+    for (int i = 0; i < text.length(); i++) {
+      char c = text.charAt(i);
+      if (Character.isLetter(c)) {
+        char numberOfCyrillicLetters = Character.isUpperCase(c) ? 'А' : 'а';
+        char numberOfLatinLetters = Character.isUpperCase(c) ? 'A' : 'a';
 
 
 }
