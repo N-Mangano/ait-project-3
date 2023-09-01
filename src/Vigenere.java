d13b43ec960c3c9df0725e9fdcd4f4a578b3855e
Nadezda Mangano <nadenka24@yandex.ru>
2023-09-01 21:34:00 +0200

    fix: add encrypt method Cyrillic

--- src/Vigenere.java
+++ src/Vigenere.java
@@ -13,6 +13,13 @@ public class Vigenere {
       if (Character.isLetter(c)) {
         char numberOfCyrillicLetters = Character.isUpperCase(c) ? 'А' : 'а';
         char numberOfLatinLetters = Character.isUpperCase(c) ? 'A' : 'a';
+        // если КИРИЛЛИЦА
+        if (c>1040) {
+          int offset = (int) (c - numberOfCyrillicLetters);
+          int keyChar = key.charAt(keyIndex) - 'А';
+          char encryptedChar = (char) ((offset + keyChar) % 32 + numberOfCyrillicLetters);
+          encryptedText.append(encryptedChar);
+          keyIndex = (keyIndex + 1) % key.length();
 
 
 }
